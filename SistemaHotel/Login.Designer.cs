10c7209e698c8987c06f2a50531056e645849d14
Miguel Pereira <miguelnewsp@hotmail.com>
2021-03-30 16:14:44 -0300

    Propriedades dos Formularios

--- SistemaHotel/Login.Designer.cs
+++ SistemaHotel/Login.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace SistemaHotel
 {
-    partial class Login
+    partial class FrmLogin
     {
         /// <summary>
         /// Required designer variable.
@@ -29,24 +29,38 @@ namespace SistemaHotel
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.pnlLogin = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // Login
+            // pnlLogin
+            // 
+            this.pnlLogin.Location = new System.Drawing.Point(203, 37);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(332, 340);
+            this.pnlLogin.TabIndex = 0;
+            // 
+            // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(784, 450);
+            this.Controls.Add(this.pnlLogin);
             this.DoubleBuffered = true;
-            this.Name = "Login";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmLogin";
             this.Text = "Login";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlLogin;
     }
 }
 
