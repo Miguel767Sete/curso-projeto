f9a0f37bed5b0715c8ee2b9c448a08d1dffc7097
Miguel Pereira <miguelnewsp@hotmail.com>
2021-04-06 15:43:54 -0300

    Criando menu principal

--- SistemaHotel/Menu.Designer.cs
+++ SistemaHotel/Menu.Designer.cs
@@ -29,23 +29,132 @@ namespace SistemaHotel
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuCadastros = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuProdutos = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuServicos = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuMovimentacoes = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuReservas = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuRelatorios = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuSair = new System.Windows.Forms.ToolStripMenuItem();
+            this.funcionáriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hóspedesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quartosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuCadastros,
+            this.MenuProdutos,
+            this.MenuServicos,
+            this.MenuMovimentacoes,
+            this.MenuReservas,
+            this.MenuRelatorios,
+            this.MenuSair});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenuCadastros
+            // 
+            this.MenuCadastros.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.funcionáriosToolStripMenuItem,
+            this.hóspedesToolStripMenuItem,
+            this.quartosToolStripMenuItem});
+            this.MenuCadastros.Name = "MenuCadastros";
+            this.MenuCadastros.Size = new System.Drawing.Size(71, 20);
+            this.MenuCadastros.Text = "Cadastros";
+            // 
+            // MenuProdutos
+            // 
+            this.MenuProdutos.Name = "MenuProdutos";
+            this.MenuProdutos.Size = new System.Drawing.Size(67, 20);
+            this.MenuProdutos.Text = "Produtos";
+            // 
+            // MenuServicos
+            // 
+            this.MenuServicos.Name = "MenuServicos";
+            this.MenuServicos.Size = new System.Drawing.Size(62, 20);
+            this.MenuServicos.Text = "Serviços";
+            // 
+            // MenuMovimentacoes
+            // 
+            this.MenuMovimentacoes.Name = "MenuMovimentacoes";
+            this.MenuMovimentacoes.Size = new System.Drawing.Size(104, 20);
+            this.MenuMovimentacoes.Text = "Movimentações";
+            // 
+            // MenuReservas
+            // 
+            this.MenuReservas.Name = "MenuReservas";
+            this.MenuReservas.Size = new System.Drawing.Size(64, 20);
+            this.MenuReservas.Text = "Reservas";
+            // 
+            // MenuRelatorios
+            // 
+            this.MenuRelatorios.Name = "MenuRelatorios";
+            this.MenuRelatorios.Size = new System.Drawing.Size(71, 20);
+            this.MenuRelatorios.Text = "Relatórios";
+            // 
+            // MenuSair
+            // 
+            this.MenuSair.Name = "MenuSair";
+            this.MenuSair.Size = new System.Drawing.Size(38, 20);
+            this.MenuSair.Text = "Sair";
+            // 
+            // funcionáriosToolStripMenuItem
+            // 
+            this.funcionáriosToolStripMenuItem.Name = "funcionáriosToolStripMenuItem";
+            this.funcionáriosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.funcionáriosToolStripMenuItem.Text = "Funcionários";
+            // 
+            // hóspedesToolStripMenuItem
+            // 
+            this.hóspedesToolStripMenuItem.Name = "hóspedesToolStripMenuItem";
+            this.hóspedesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hóspedesToolStripMenuItem.Text = "Hóspedes";
+            // 
+            // quartosToolStripMenuItem
+            // 
+            this.quartosToolStripMenuItem.Name = "quartosToolStripMenuItem";
+            this.quartosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quartosToolStripMenuItem.Text = "Quartos";
             // 
             // FrmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "FrmMenu";
-            this.Text = "Menu";
+            this.Text = "Hotel";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Resize += new System.EventHandler(this.FrmMenu_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenuCadastros;
+        private System.Windows.Forms.ToolStripMenuItem MenuProdutos;
+        private System.Windows.Forms.ToolStripMenuItem MenuServicos;
+        private System.Windows.Forms.ToolStripMenuItem MenuMovimentacoes;
+        private System.Windows.Forms.ToolStripMenuItem MenuReservas;
+        private System.Windows.Forms.ToolStripMenuItem MenuRelatorios;
+        private System.Windows.Forms.ToolStripMenuItem MenuSair;
+        private System.Windows.Forms.ToolStripMenuItem funcionáriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hóspedesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quartosToolStripMenuItem;
     }
 }
