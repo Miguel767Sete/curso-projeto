7b9ef8163496e72eec755ad9a7de23ae1b39608b
Miguel Pereira <miguelnewsp@hotmail.com>
2021-04-06 22:29:32 -0300

    Finalizando os menus

--- SistemaHotel/Menu.Designer.cs
+++ SistemaHotel/Menu.Designer.cs
@@ -58,7 +58,15 @@ namespace SistemaHotel
             this.pnlRigtht = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
+            this.pnlTopo.SuspendLayout();
             this.pnlRigtht.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -74,7 +82,7 @@ namespace SistemaHotel
             this.MenuSair});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(928, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -251,9 +259,16 @@ namespace SistemaHotel
             this.pnlTopo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlTopo.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pnlTopo.Controls.Add(this.button7);
+            this.pnlTopo.Controls.Add(this.button6);
+            this.pnlTopo.Controls.Add(this.button5);
+            this.pnlTopo.Controls.Add(this.button4);
+            this.pnlTopo.Controls.Add(this.button3);
+            this.pnlTopo.Controls.Add(this.button2);
+            this.pnlTopo.Controls.Add(this.button1);
             this.pnlTopo.Location = new System.Drawing.Point(0, 27);
             this.pnlTopo.Name = "pnlTopo";
-            this.pnlTopo.Size = new System.Drawing.Size(619, 75);
+            this.pnlTopo.Size = new System.Drawing.Size(747, 75);
             this.pnlTopo.TabIndex = 1;
             // 
             // pnlRigtht
@@ -263,9 +278,9 @@ namespace SistemaHotel
             this.pnlRigtht.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.pnlRigtht.Controls.Add(this.label2);
             this.pnlRigtht.Controls.Add(this.label1);
-            this.pnlRigtht.Location = new System.Drawing.Point(615, 27);
+            this.pnlRigtht.Location = new System.Drawing.Point(743, 27);
             this.pnlRigtht.Name = "pnlRigtht";
-            this.pnlRigtht.Size = new System.Drawing.Size(193, 425);
+            this.pnlRigtht.Size = new System.Drawing.Size(314, 425);
             this.pnlRigtht.TabIndex = 2;
             // 
             // label1
@@ -290,11 +305,102 @@ namespace SistemaHotel
             this.label2.TabIndex = 1;
             this.label2.Text = "Hora:";
             // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(3, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 65);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(97, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 65);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(192, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 65);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(296, 7);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 65);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(390, 7);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 65);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(482, 7);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(70, 65);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(580, 7);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(70, 65);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
             // FrmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(928, 450);
             this.Controls.Add(this.pnlRigtht);
             this.Controls.Add(this.pnlTopo);
             this.Controls.Add(this.menuStrip1);
@@ -308,6 +414,7 @@ namespace SistemaHotel
             this.Resize += new System.EventHandler(this.FrmMenu_Resize);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.pnlTopo.ResumeLayout(false);
             this.pnlRigtht.ResumeLayout(false);
             this.pnlRigtht.PerformLayout();
             this.ResumeLayout(false);
@@ -345,5 +452,12 @@ namespace SistemaHotel
         private System.Windows.Forms.Panel pnlRigtht;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
     }
 }
